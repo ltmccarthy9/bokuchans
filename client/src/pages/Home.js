22309129f7855d21b1acbf8f4daff1fef8508863
Liam McCarthy <ltmccarthy9@gmail.com>
2022-10-06 17:30:31 -0500

    color change

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            <h1>Checkout Our Sauces on Amazon!</h1>
+            <h3>Check Out Our Sauces on Amazon!</h3>
         </div>
     );
 }
