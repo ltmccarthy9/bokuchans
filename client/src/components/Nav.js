82c73c119b605fb333e2749455bb8afb6f28b6d6
Liam McCarthy <ltmccarthy9@gmail.com>
2022-10-06 14:28:36 -0500

    nav bar

---
+++ client/src/components/Nav.js
@@ -0,0 +1,22 @@
+
+const Nav = () => {
+    return (
+        <div className="banner">
+            <div className="navcard">
+                <h1 className="title">Bokuchan's</h1>
+            </div>
+            <div className="navcard">
+                <h3>Sauces & Foods</h3>
+            </div>
+            <div className="navcard">
+                <h3>Japanese Curry House</h3>
+            </div>
+            <div className="navcard">
+                <h3>Events & Additional Info</h3>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Nav;
